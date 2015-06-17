38bb4fd70219c34030fcadb0cfc7e6ce2d9e6d03
hpetrow <heather.petrow@gmail.com>
2015-06-17 14:08:14 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 6, 'M', 'brown', 'friendly', 1),
+('Rowdy', 4, 'M', 'white', 'fun', 0),
+('Tabitha', 2, 'F', 'blue', 'fun', 1),
+('Sergeant Brown', 6, 'M', 'brown', 'serious', 0),
+('Melissa', 2, 'F', 'white', 'friendly', 1),
+('Grinch', 6, 'M', 'brown', 'serious', 0),
+('Wendy', 4, 'F', 'blue', 'serious', 1);
+
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES
+(4, 'M', 'blue', 'mean', 1);
